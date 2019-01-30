f07e9c6e815b9e97eeb714f20953d88149b36fc4
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-01-29 16:40:06 -0800

    randomly generates sentences based on file words

--- Code/dictonary_words.py
+++ Code/dictonary_words.py
@@ -5,14 +5,15 @@ number = input("Enter a number: ")
 def sentence_generator(num):
     random_words = list()
 
-    while int(num) > len(random_words):
-        f = open('/usr/share/dict/words')
-        text = f.readlines()
-        max = random.randint(0,len(text)-1)
-        randword = text[max]
-        random_words.append(randword)
-        f.close()
-    # check for duplicates?
-    return "".join(random_words)
+    with open('/usr/share/dict/words') as f:
+        text = f.read().splitlines() 
+
+        while int(num) > len(random_words):
+            max = random.randint(0,len(text)-1)
+            randword = text[max]
+            random_words.append(randword)
+        # check for duplicates?
+    
+    return " ".join(random_words) + '.'
 
 print(sentence_generator(number))
