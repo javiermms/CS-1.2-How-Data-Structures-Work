4da1bf4ac69e5df29762bd5601e9667bfe7392e0
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-01-30 20:47:21 -0800

    refactor dictonary histogram

--- Code/histogram_dic.py
+++ Code/histogram_dic.py
@@ -1,18 +1,13 @@
 with open('sample.txt') as f:
-    array = {}
+    dic = {}
     text = f.read()
     words_array = text.split()
 
-    def frequency(word):
-        frequency = 0
-        
-        for match in words_array:
-            if word == match:
-                frequency += 1
-        return frequency
-   
-    for i in range(0, len(words_array) - 1):
-        for j in range(0, len(words_array) - 1):
-            array[words_array[i]] = frequency(words_array[i])
+ 
+    for word in words_array:
+        try:
+            dic[word] = dic[word] + 1
+        except KeyError:
+            dic[word] = 1
 
-print(array)
+print(dic)
---
+++ Code/histogram_tuple.py
@@ -0,0 +1,19 @@
+with open('sample.txt') as f:
+    array = []
+    tupel = ()
+    text = f.read()
+    words_array = text.split()
+
+    def frequency(word):
+        frequency = 0
+        
+        for match in words_array:
+            if word == match:
+                frequency += 1
+        return frequency
+   
+    for i in range(0, len(words_array) - 1):
+        array.append(tupel + (words_array[i], frequency(words_array[i])))
+        
+
+print(array)
