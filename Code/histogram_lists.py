e9c83149a6780d1bd1097606460274d1657123fe
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-02-07 12:34:59 -0800

    better python implementation

--- Code/histogram_lists.py
+++ Code/histogram_lists.py
@@ -3,30 +3,19 @@ array = []
 with open('sample.txt') as f:
     text = f.read()
     words_array = text.split()
-    filter_words = set(words_array)
-
-    for word in filter_words:
-        array.append([word])
-
-    def frequency(word):
-        frequency = 0
-        
-        for match in words_array:
-            if word == match:
-                frequency += 1
-        return frequency
     
-    for i in range(0, len(array)):
-        for j in range(0, len(array[i])):
-            array[i].append(frequency(array[i][j]))
+    for word in words_array:
+        found = False                       
+        for element in array:
+            if word in element[0]:
+                found = True
+                element[1] += 1
+                break
 
-    # for word in word-list:
-        # for inner_list in list:
-        #     if word == inner_list[0]
-        #         inner_list[1] += 1
-        # OR      found = True
-        #         break
-                #the outer for loop
-        #   if not found:
-            # list.append([word,1])
+        if not found:
+            array.append([word, 1])
+
+print(array)
+
+
 
--- Code/stochastic_ sampling.py
+++ Code/stochastic_ sampling.py
@@ -1,13 +1,18 @@
-from histogram_lists import *
+from histogram_lists import array
 import random
+
+# modeled after https://github.com/Mintri1199/Tweet_Generator/blob/master/stochastic_sample.py
+# I will change the histogram to list of counts once I complete the list of counts challenge
 
 def probability(histogram):
     total_freq = 0
-    cumulative_probability = 0
+    cumulative_probability = 0.0
 
+    # Adds up all word frequency from the histogram
     for element in histogram:
         total_freq += element[1]
 
+    # picks a random word weighted through frequency
     random_num = random.uniform(0, 1)
     for element in histogram:
         cumulative_probability += element[1]/total_freq
@@ -17,13 +22,15 @@ def probability(histogram):
 def check_accuracy(histogram):
     proof_dict = dict()
 
+    # fills a dictionary with a word as a key and sets probability to 0 
     for element in histogram:
         proof_dict[element[0]] = 0
-
+    # stores a sample of 1000 function calls
     for _ in range(0, 1000):
         proof_dict[probability(histogram)] += 1
 
-    print(proof_dict)
+    return(proof_dict)
 
-print(probability(array))
-check_accuracy(array)
+# print(array)
+# print(probability(array))
+print(check_accuracy(array))
