571f4a1341f5dd346cb50f3b11131825d078f26f
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-02-28 10:11:54 -0800

    add_count & frequency

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -20,10 +20,20 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word:
+            self.tokens = count 
+        else:
+            self[word] = count
+            self.types += 1
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+
+        if word in self:
+            return self[word]
+        else: 
+            return 0
 
 
 def print_histogram(word_list):
